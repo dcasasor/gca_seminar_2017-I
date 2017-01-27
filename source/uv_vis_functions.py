cc96550a07ae33f3b0b58218ddec59a34eb8c1ba
dcasasor <daniel.casas@udea.edu.co>
2017-01-27 18:25:12 -0500

    Changed module import from functions to uvfuncs. Cleaned tex auxiliary files

--- source/uv_vis_functions.py
+++ source/uv_vis_functions.py
@@ -14,8 +14,8 @@ def process_data(raw_data, scaling_factors):
     Inputs
     ----------
     raw_data : array_type
-        UV-Vis raw data organized in columns. First column contains wavelength
-        values (nm) and tramitance values (au.) from second column on .
+        UV-Vis raw data organized in columns. Odd colums contain wavelength
+        values (nm) and even columns contain tramitance values (au.).
 
     scaling_factors : tuple
         Contains numbers in the range 0 - 1 which are intended to multiply
@@ -49,12 +49,12 @@ def plot_uv_vis(Eg_array, tramitance_array, offset):
     import matplotlib.pyplot as plt
     from matplotlib.ticker import AutoMinorLocator
 
+    # Create figure
     fig, axis = plt.subplots()
     number_plots = len(Eg_array.T)
 
-    offset_acum = 0
-
     # --------------- Iterate to plot all curves
+    offset_acum = 0
 
     for ind in range(number_plots):
         axis.plot(Eg_array[:, ind], tramitance_array[:, ind] + offset_acum)
--- source/uv_vis_launcher.py
+++ source/uv_vis_launcher.py
@@ -1,5 +1,5 @@
 import numpy as np
-import uv_vis_functions as functions
+import uv_vis_functions as uvfuncs
 
 
 # --------------- Import data
@@ -9,19 +9,19 @@ raw_uv_data = np.genfromtxt('../data/uv_vis_data.csv', delimiter=',',
 # --------------- Process data
 
 scaling_fc = (1, 1, 1, 1, 1)
-Eg_data, tramitance_sc = functions.process_data(raw_uv_data, scaling_fc)
+Eg_data, tramitance_sc = uvfuncs.process_data(raw_uv_data, scaling_fc)
 
-# Export data
+# Export processed data
 np.savetxt('../results/tables/Eg_data.csv', Eg_data, delimiter=',')
 np.savetxt('../results/tables/tramitance_sc.csv', tramitance_sc, delimiter=',')
 
 # --------------- Plot data
 offset = 20
-fig_uv, axis_uv = functions.plot_uv_vis(Eg_data, tramitance_sc, offset)
+fig_uv, axis_uv = uvfuncs.plot_uv_vis(Eg_data, tramitance_sc, offset)
 
 # Edit axes
 axis_uv.set_title('Example of UV Vis plotting for GCA Seminar')
-axis_uv.set_xlim(xmax=6.2)
+axis_uv.set_xlim(3, 6.2)
 axis_uv.set_ylim(ymax=110)
 axis_uv.yaxis.set_ticks([])  # Remove yticks
 
@@ -29,7 +29,7 @@ axis_uv.yaxis.set_ticks([])  # Remove yticks
 names_curves = ['Material %i' % ind for ind in range(1, 6)]
 offset_acum = 1
 for name in names_curves:
-    axis_uv.text(1.2, offset_acum, name)
+    axis_uv.text(3.2, offset_acum, name)
     offset_acum += offset
 
 # Save figure
